1428fb2f811ee8525b10f0e3844b062b00f36f24
200411100061 - Diah Kamalia <101076428+diahkamalia@users.noreply.github.com>
2023-06-15 15:41:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,30 @@ with st.container():
                 
         with preprocessing : 
             st.write("""# Preprocessing""")
-            
+            # transform univariate time series to supervised learning problem
+            from numpy import array
+            # split a univariate sequence into samples
+            def split_sequence(sequence, n_steps):
+                X, y = list(), list()
+                for i in range(len(sequence)):
+                # find the end of this pattern
+                    end_ix = i + n_steps
+                # check if we are beyond the sequence
+                    if end_ix > len(sequence)-1:
+                        break
+                # gather input and output parts of the pattern
+                    # print(i, end_ix)
+                    seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+                    X.append(seq_x)
+                    y.append(seq_y)
+                return array(X), array(y)
+            n_steps = 5
+            X, y = split_sequence(data['Open'], n_steps)
+            X.shape, y.shape
+            st.info("#### Normalisasi ")
+            scaler = MinMaxScaler(feature_range=(0,1))
+            scaled = scaler.fit_transform(X)
+            scaled
 
         with model : 
             st.write("""# Model""")
